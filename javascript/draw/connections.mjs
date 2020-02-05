5a2e5f305b93ef894563b80efe4fe42c016f8dfc
Jonathan Marsh <hello@jonmarsh.dev>
2020-02-05 19:50:50 +0000

    JF-6: Added self connections

--- javascript/draw/connections.mjs
+++ javascript/draw/connections.mjs
@@ -10,12 +10,79 @@ export let downOnNode = false
 
 const ARROW_SIZE = 10
 const HANDLE_SIZE = 50
+const SELF_LOOP_SIZE = 100
 
 function drawArrow(x, y, angle) {
   context.moveTo(x, y)
   context.lineTo(x - ARROW_SIZE * zoom * Math.cos(angle - Math.PI / 6), y - ARROW_SIZE * zoom * Math.sin(angle - Math.PI / 6))
   context.moveTo(x, y)
   context.lineTo(x - ARROW_SIZE * zoom * Math.cos(angle + Math.PI / 6), y - ARROW_SIZE * zoom * Math.sin(angle + Math.PI / 6))
+}
+
+function drawSelfConnection(fromNode) {
+  const startX = connectionArray[fromNode].x * zoom + centre.x
+  const startY = connectionArray[fromNode].y * zoom + centre.y
+  
+  context.moveTo(startX, startY)
+  context.bezierCurveTo(startX - SELF_LOOP_SIZE * zoom, startY - SELF_LOOP_SIZE * zoom, startX + SELF_LOOP_SIZE * zoom, startY - SELF_LOOP_SIZE * zoom, startX, startY);
+}
+
+function drawOneWayConnection(fromNode, toNode) {
+  const startX = connectionArray[fromNode].x * zoom + centre.x
+  const startY = connectionArray[fromNode].y * zoom + centre.y
+  
+  const endX = connectionArray[fromNode][toNode].x * zoom + centre.x
+  const endY = connectionArray[fromNode][toNode].y * zoom + centre.y
+  
+  const midX = startX + (endX - startX) / 2
+  const midY = startY + (endY - startY) / 2
+  
+  const deltaX = endX - startX
+  const deltaY = endY - startY
+  
+  const angle = Math.atan2(deltaY, deltaX)
+
+  context.moveTo(startX, startY)
+  context.lineTo(endX, endY)
+  drawArrow(midX, midY, angle)
+}
+
+function drawTwoWayConnection(fromNode, toNode) {
+  const startX = connectionArray[fromNode].x * zoom + centre.x
+  const startY = connectionArray[fromNode].y * zoom + centre.y
+  
+  const endX = connectionArray[fromNode][toNode].x * zoom + centre.x
+  const endY = connectionArray[fromNode][toNode].y * zoom + centre.y
+  
+  const midX = startX + (endX - startX) / 2
+  const midY = startY + (endY - startY) / 2
+  
+  const deltaX = endX - startX
+  const deltaY = endY - startY
+  
+  const angle = Math.atan2(deltaY, deltaX)
+  const perpendicularAngle = Math.atan2(-deltaX, deltaY)
+  
+  const handleX = HANDLE_SIZE * zoom * Math.cos(perpendicularAngle)
+  const handleY = HANDLE_SIZE * zoom * Math.sin(perpendicularAngle)
+        
+  context.moveTo(startX, startY)
+  context.quadraticCurveTo(midX + handleX, midY + handleY, endX, endY)
+  
+  drawArrow(midX + handleX / 2, midY + handleY / 2, angle)
+}
+
+function drawConnection(fromNode, toNode) {
+  if (fromNode === toNode) {
+    drawSelfConnection(fromNode)
+  }
+  else {
+    if (connectionArray[toNode] && connectionArray[toNode][fromNode]) {
+      drawTwoWayConnection(fromNode, toNode)
+    } else {
+      drawOneWayConnection(fromNode, toNode)
+    }
+  }
 }
 
 export function draw() {
@@ -25,35 +92,7 @@ export function draw() {
 
   for (let fromNode in connectionArray) {
     for (let toNode in connectionArray[fromNode]) {
-      const startX = connectionArray[fromNode].x * zoom + centre.x
-      const startY = connectionArray[fromNode].y * zoom + centre.y
-      
-      const endX = connectionArray[fromNode][toNode].x * zoom + centre.x
-      const endY = connectionArray[fromNode][toNode].y * zoom + centre.y
-      
-      const midX = startX + (endX - startX) / 2
-      const midY = startY + (endY - startY) / 2
-      
-      const deltaX = endX - startX
-      const deltaY = endY - startY
-      
-      const angle = Math.atan2(deltaY, deltaX)
-            
-      context.moveTo(startX, startY)
-      if (connectionArray[toNode] && connectionArray[toNode][fromNode]) {
-        const perpendicularAngle = Math.atan2(-deltaX, deltaY)
-        const handleX = HANDLE_SIZE * zoom * Math.cos(perpendicularAngle)
-        const handleY = HANDLE_SIZE * zoom * Math.sin(perpendicularAngle)
-      
-        context.moveTo(startX, startY)
-        context.quadraticCurveTo(midX + handleX, midY + handleY, endX, endY)
-        
-        drawArrow(midX + handleX / 2, midY + handleY / 2, angle)
-      } else {
-        context.moveTo(startX, startY)
-        context.lineTo(endX, endY)
-        drawArrow(midX, midY, angle)
-      }
+      drawConnection(fromNode, toNode)
     }  
   }
   if (nodeOne && !nodeTwo) {
@@ -62,12 +101,6 @@ export function draw() {
   }
   context.stroke()
 }
-
-mouse.subscribe(mouse.EVENT_TYPE.UP_LEFT, () => {
-  if (!hoveringOverNode) {
-    
-  }
-})
 
 mouse.subscribe(mouse.EVENT_TYPE.DOWN_LEFT, () => {
   downOnNode = hoveringOverNode
