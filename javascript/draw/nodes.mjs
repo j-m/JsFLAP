48960f3cb18ca38581f6243d962351a344f64722
Jonathan Marsh <hello@jonmarsh.dev>
2021-08-08 01:27:04 +0100

    feat(node): add bubble for selected node

--- javascript/draw/nodes.mjs
+++ javascript/draw/nodes.mjs
@@ -5,12 +5,11 @@ import { GRID_STEP } from "../settings/application.mjs"
 import { SNAP_NODES } from "../settings/user.mjs"
 import { connectionArray, connectFromNode, setConnectFromNode } from "./connections.mjs"
 
-export let nodeOne
-export let nodeTwo
 export let hoveringOverNode = null
 export let selectedNode = null
 
 const NODE_RADIUS = GRID_STEP * 0.5
+const NODE_OUTLINE_WIDTH = 5
 
 let nodeArray = []
 
@@ -19,7 +18,7 @@ function drawNode(node) {
   context.arc(node.x * zoom + centre.x, node.y * zoom + centre.y, NODE_RADIUS * zoom, 0, 2 * Math.PI, false)
   context.fillStyle = nodeIsHighlighted(node) ? 'white' : node.fill
   context.fill()
-  context.lineWidth = 5 * zoom
+  context.lineWidth = NODE_OUTLINE_WIDTH * zoom
   context.strokeStyle = nodeIsHighlighted(node) ? 'black' : node.outline
   context.stroke()
 
@@ -28,18 +27,73 @@ function drawNode(node) {
   context.fillText("s" + node.id, node.x * zoom + centre.x, node.y * zoom + centre.y);
 }
 
+const BUBBLE_WIDTH = 120
+const BUBBLE_HEIGHT = 40
+const BUBBLE_ARROW = 10
+const BUBBLE_RADIUS = 5
+const BUBBLE_OUTLINE_WIDTH = 3
+
+function drawBubble(x, y) {
+  y = y - NODE_RADIUS * zoom - NODE_OUTLINE_WIDTH / 2 * zoom
+
+  context.strokeStyle = "none";
+  context.fillStyle = "rgba(0, 0, 0, 0.5)";
+
+  context.beginPath();
+  context.moveTo(x, y);
+  context.lineTo(x + BUBBLE_ARROW * zoom, y - BUBBLE_ARROW * zoom)
+  context.lineTo(x + BUBBLE_WIDTH * zoom / 2 - BUBBLE_RADIUS * zoom, y - BUBBLE_ARROW * zoom)
+  context.quadraticCurveTo(
+    x + BUBBLE_WIDTH * zoom / 2,
+    y - BUBBLE_ARROW * zoom,
+    x + BUBBLE_WIDTH * zoom / 2,
+    y - BUBBLE_ARROW * zoom - BUBBLE_RADIUS * zoom
+  )
+  context.lineTo(x + BUBBLE_WIDTH * zoom / 2, y - BUBBLE_ARROW * zoom - BUBBLE_HEIGHT * zoom + BUBBLE_RADIUS * zoom)
+  context.quadraticCurveTo(
+    x + BUBBLE_WIDTH * zoom / 2,
+    y - BUBBLE_ARROW * zoom - BUBBLE_HEIGHT * zoom,
+    x + BUBBLE_WIDTH * zoom / 2 - BUBBLE_RADIUS * zoom,
+    y - BUBBLE_ARROW * zoom - BUBBLE_HEIGHT * zoom
+  )
+  context.lineTo(x - BUBBLE_WIDTH * zoom / 2 + BUBBLE_RADIUS * zoom, y - BUBBLE_ARROW * zoom - BUBBLE_HEIGHT * zoom)
+  context.quadraticCurveTo(
+    x - BUBBLE_WIDTH * zoom / 2,
+    y - BUBBLE_ARROW * zoom - BUBBLE_HEIGHT * zoom,
+    x - BUBBLE_WIDTH * zoom / 2,
+    y - BUBBLE_ARROW * zoom - BUBBLE_HEIGHT * zoom + BUBBLE_RADIUS * zoom
+  )
+  context.lineTo(x - BUBBLE_WIDTH * zoom / 2, y - BUBBLE_ARROW * zoom - BUBBLE_RADIUS * zoom)
+  context.quadraticCurveTo(
+    x - BUBBLE_WIDTH * zoom / 2,
+    y - BUBBLE_ARROW * zoom,
+    x - BUBBLE_WIDTH * zoom / 2 + BUBBLE_RADIUS * zoom,
+    y - BUBBLE_ARROW * zoom
+  )
+  context.lineTo(x - BUBBLE_ARROW * zoom, y - BUBBLE_ARROW * zoom)
+  context.closePath()
+  context.fill()
+}
+
+function drawActions() {
+  if (!selectedNode) {
+    return
+  }
+  drawBubble(selectedNode.x * zoom + centre.x, selectedNode.y * zoom + centre.y)
+}
+
 export function draw() {
   context.font = `${NODE_RADIUS * zoom}px Arial`
   context.textAlign = "center"
   nodeArray.forEach(node => {
     drawNode(node)
   })
+  drawActions()
 }
 
 function nodeIsHighlighted(node) {
   return node === hoveringOverNode
-    || node === nodeOne
-    || node === nodeTwo
+    || node === selectedNode
 }
 
 function snapNodePositionIfEnabled(x, y) {
@@ -51,7 +105,7 @@ function snapNodePositionIfEnabled(x, y) {
 }
 
 function createNode() {
-  if (!hoveringOverNode && !mouse.hasMovedSinceDown) {
+  if (!selectedNode && !hoveringOverNode && !mouse.hasMovedSinceDown) {
     const [x, y] = snapNodePositionIfEnabled((mouse.x - centre.x) / zoom, (mouse.y - centre.y) / zoom)
     nodeArray.push(new Node(x, y))
   }
@@ -98,22 +152,27 @@ function finishConnecting() {
   setConnectFromNode(null)
 }
 
+function selectNode() {
+  if (hoveringOverNode && !hasMouseLeftNode) {
+    selectedNode = hoveringOverNode
+  }
+}
+
+function deselectNode() {
+  if (!hoveringOverNode) {
+    selectedNode = null
+  }
+}
+
 document.getElementById("SNAP_EXISTING_NODES").addEventListener("click", () => {
   nodeArray.forEach(node => {
     [node.x, node.y] = snapNodePositionIfEnabled(node.x, node.y)
   })
 })
 
-document.getElementById("node1Delete").addEventListener("click", () => {
-  nodeOne = null
-})
-
-document.getElementById("node2Delete").addEventListener("click", () => {
-  nodeTwo = null
-})
-
 mouse.subscribe(mouse.EVENT_TYPE.UP_LEFT, createNode)
+mouse.subscribe(mouse.EVENT_TYPE.UP_LEFT, deselectNode)
 mouse.subscribe(mouse.EVENT_TYPE.MOVE, whichNodeIsMouseHoveringOver)
 mouse.subscribe(mouse.EVENT_TYPE.DOWN_LEFT, startConnecting)
 mouse.subscribe(mouse.EVENT_TYPE.UP_LEFT, finishConnecting)
-
+mouse.subscribe(mouse.EVENT_TYPE.UP_LEFT, selectNode)
